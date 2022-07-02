f1e65b31e75d793b6d99ebcc3a8f42fb2025ad75
Ekaterina <katya.leemi@gmail.com>
2022-07-02 23:06:18 +0300

    Refactored App.js, changed from React.Component to Arrow Func

--- src/App.js
+++ src/App.js
@@ -1,61 +1,39 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import TasksList from './TasksList';
 import Input from './Input';
 
-const listArray = [
+const defaultState = [
   {id: 0, content: "This is an important task", done: false},
   {id: 1, content: "This is alse an important task", done: false},
   {id: 2, content: "This is slightly less an important task", done: false},
 ]
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { listArray, idCounter: 3 };
+const App = () => {
+  const [ tasks, setTasks ] = useState(defaultState)
+  const [ idCount, setIdCount ] = useState(3);
+
+  const deleteTask = (id) => {
+    setTasks(oldTasks => oldTasks.filter(obj => obj.id !== id))
   }
-  
-  onSubmit = (newTask) => {
-    let id = this.state.idCounter;
+
+  const checkTask = (id, isChecked) => {
+    setTasks(oldTasks => oldTasks.map(obj => obj.id === id ? { ...obj, done: isChecked} : obj))
+  }
+
+  const onSubmit = (newTask) => {
+    let id = idCount;
     let newObj = {id, content: newTask, done: false};
-    this.setState(state => {
-      return { listArray: [...state.listArray, newObj], idCounter: state.idCounter + 1}
-    });
+    setTasks(oldTasks => [...oldTasks, newObj]);
+    setIdCount(oldIdCount => oldIdCount + 1)
   }
 
-  deleteTask = (id) => {
-    this.setState(state => {
-      return {
-        listArray: state.listArray.filter(obj => obj.id !== id)
-      }
-    })
-  }
-
-  checkTask = (id, isChecked) => {
-    this.setState(state => {
-      return {
-        listArray: state.listArray.map(obj => {
-              if(obj.id === id){
-                return {
-                  ...obj,
-                  done: isChecked
-                } 
-              } else {
-                return obj
-              }
-            })
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Input onSubmit={this.onSubmit}/>
-        <TasksList tasks={this.state.listArray} deleteTask={this.deleteTask} checkTask={this.checkTask}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Input onSubmit={onSubmit}/>
+      <TasksList tasks={tasks} deleteTask={deleteTask} checkTask={checkTask}/>
+    </div>
+  )
 }
 
 export default App;
