9589e65a8754e722d07f37d8ddcbc7de387872ce
Ekaterina <katya.leemi@gmail.com>
2022-07-02 23:09:43 +0300

    Changed Tasks and TasksList to arrow functions as well

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Task.module.css';
 
-function Task(props){
+const Task = (props) => {
     const onChange = (e) => {
         props.checkTask(props.task.id, e.target.checked);
     };
--- src/TasksList.js
+++ src/TasksList.js
@@ -1,6 +1,6 @@
 import Task from './Task';
 
-function TasksList(props) {
+const TasksList = (props) => {
     return (
       <div style={{ textAlign: 'center', marginLeft: 100 }}>
           {props.tasks.map(task=><Task task={task} key={task.id} deleteTask={props.deleteTask} checkTask={props.checkTask}/>)}
