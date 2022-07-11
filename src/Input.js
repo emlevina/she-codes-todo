73faaee413ddbfa9a162a66ff4f5be6a12140f50
Ekaterina <katya.leemi@gmail.com>
2022-07-11 15:37:18 +0300

    revert

--- src/Input.js
+++ src/Input.js
@@ -6,14 +6,35 @@ import { addTask } from './actions';
 
 const Input = (props) => {
     const [ input, setInput ] = useState('')
+
+    const onFocus = (e) => {
+        if(!input){
+            setInput('$')
+            e.currentTarget.setSelectionRange(input.length+1, input.length+1) 
+        }
+    }
     
+    const onBlur = () => {
+        if(input.length === 1){
+            setInput('') //чтобы пропадал значок, если ничего так и не ввели
+        }
+    }
+
     const onChange = (e) => {
-        setInput(e.target.value)
+        if(e.target.value === '$'){
+            setInput('') //на случай когда удаляют последнюю цифру, чтобы доллар пропал. хотя мб это и не надо
+        } else if (e.target.value.length === 1){
+            setInput('$' + e.target.value) // на случай когда ручками удаляют, а потом продолжают печатать
+        } else {
+            setInput(e.target.value)
+        }
+        
     }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        props.addTask(input)
+        //setInput(input.slice(1))
+        props.addTask(input.slice(1))
         setInput('')
     }
 
@@ -22,13 +43,14 @@ const Input = (props) => {
             <input 
             value={input} 
             onChange={onChange} 
+            onFocus={onFocus}
+            onBlur={onBlur}
             className={styles.inputTask} 
             placeholder='Type your task here'/>
             <button className={styles.button} >submit</button>
         </form>
     )
     
-   
 }
 
 const mapDispatchToProps = (dispatch) => {
